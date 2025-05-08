e16dffb119938efeda09457280739f35ac3b996d
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-05-08 20:32:11 +0100

    add comments to files

--- app/models.py
+++ app/models.py
@@ -1,14 +1,16 @@
 # Maps DB to tables.
+# The file is responsible for Object-Relational Mapping -
+# defining how Python classes = database tables.
 
 from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
 from app.database import Base
 
 
 class JobApplication(Base):
     __tablename__ = "job_applications"
-
+    # primary key column
     id = Column(Integer, primary_key=True, index=True)
+    # other columns
     company = Column(String, index=True)
     position = Column(String)
     location = Column(String, nullable=True)
--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,14 @@
 # Defines what data should look like (input/output).
+
+# this file tells FastAPI how data should
+# be shaped for requests (input) and responses (output).
+
 from pydantic import BaseModel
 from datetime import date
 from typing import Optional
 
-
+# Schema  which maps to DB model, minus the id.
+# shared base for create + response, reused in other schemas
 class JobAppBase(BaseModel):
     company: str
     position: str
@@ -14,10 +19,12 @@ class JobAppBase(BaseModel):
     notes: Optional[str] = None
 
 
+# Schema for incoming data (POST)
 class JobAppCreate(JobAppBase):
     pass
 
 
+# Schema for outgoing data (GET)
 class JobApp(JobAppBase):
     id: int
 
@@ -26,5 +33,8 @@ class JobApp(JobAppBase):
     }
 
 
+# For partial updates (PATCH/PUT)
+# This lets the user update only one field
+# without sending the whole object.
 class JobAppUpdate:
     pass
