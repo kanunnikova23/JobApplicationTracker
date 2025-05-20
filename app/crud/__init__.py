6b4c51189d1f7c0c5efc476a3b3893a025070a4d
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-05-20 10:50:32 +0100

    refactor(crud): expose job-related CRUD functions via __init__.py

---
+++ app/crud/__init__.py
@@ -0,0 +1,15 @@
+from .job_crud import (
+    create_job_app,
+    delete_job,
+    get_job_apps,
+    get_job_app_by_id,
+    update_job,
+)
+
+__all__ = [
+    "create_job_app",
+    "delete_job",
+    "get_job_apps",
+    "get_job_app_by_id",
+    "update_job",
+]
