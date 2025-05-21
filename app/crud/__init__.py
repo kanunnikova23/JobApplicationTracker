20bbf3df64ff850982a48df2707747475fc2aade
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-05-21 12:07:56 +0100

    feat(routes): add user_crud, update imports

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -5,6 +5,13 @@ from .job_crud import (
     get_job_app_by_id,
     update_job,
 )
+from .user_crud import (
+    create_user,
+    get_user_by_id,
+    update_user,
+    delete_user
+
+)
 
 __all__ = [
     "create_job_app",
@@ -12,4 +19,8 @@ __all__ = [
     "get_job_apps",
     "get_job_app_by_id",
     "update_job",
+    "get_user_by_id",
+    "create_user",
+    "update_user",
+    "delete_user"
 ]
---
+++ app/crud/user_crud.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+import app.models.user_models as models
+import app.schemas.user_schemas as schemas
+from app.core.logger import logger
+from app.exceptions import AppBaseException
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def create_user(db, user: schemas.UserCreate):
+    hashed_pw = pwd_context.hash(user.password)
+
+    db_user = models.User(
+        id=str(uuid4()),
+        email=user.email,
+        username=user.username,
+        full_name=user.full_name,
+        role=user.role or schemas.UserRole.USER,
+        hashed_password=hashed_pw,
+        created_at=datetime.now(),
+        last_login=None,
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    logger.info(f"✅ Created user: {db_user.username} with ID {db_user.id}")
+    return db_user
+
+
+def get_user_by_id(db: Session, user_id: UUID):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise AppBaseException()
+    return user
+
+
+def delete_user(db, user_id):
+    try:
+        user = db.query(models.User).filter(models.User.id == user_id).first()
+        if not user:
+            raise AppBaseException(status_code=404, detail="User not found.")
+        db.delete(user)
+        db.commit()
+        logger.info(f"🗑️ Deleted user with ID {user_id}")
+        return user
+    except Exception:
+        db.rollback()
+        raise AppBaseException(status_code=500, detail="Unexpected error during deletion.")
+
+
+def update_user(db, user_id, updated_data: schemas.UserUpdate):
+    try:
+        #  Fetch the job entry from the database
+        user = get_user_by_id(db, user_id)
+        #  Update only the fields that were actually passed in the request
+        for key, value in updated_data.model_dump(exclude_unset=True).items():
+            setattr(user, key, value)
+        #  Save and refresh the changes in the DB
+        db.commit()
+        db.refresh(user)
+        #  Log what got updated
+        logger.info(
+            f"✏️ Updated user with ID {user_id} with fields: {list(updated_data.model_dump(exclude_unset=True).keys())}")
+        #  Return the fully updated model
+        return user
+    except IntegrityError as e:
+        db.rollback()
+        raise AppBaseException(status_code=409, detail="Database integrity error during update.")
