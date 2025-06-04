e80d91884e82ce2a21d445074c4fc2914ce2b79d
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-06-04 13:53:54 +0100

    refactor(schema): rename JobApp to JobAppOut for clearer response model naming DB

--- app/api/routes/job_routes.py
+++ app/api/routes/job_routes.py
@@ -9,23 +9,25 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=job_schemas.JobApp, status_code=201)
+@router.post("/", response_model=job_schemas.JobAppOut, status_code=201)
 async def create_application(job: job_schemas.JobAppCreate, db: AsyncSession = Depends(get_async_db)):
     return await job_crud.create_job_app(db=db, job=job)
 
 
-@router.get("/", response_model=list[job_schemas.JobApp])
+@router.get("/", response_model=list[job_schemas.JobAppOut])
 async def read_applications(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_async_db)):
     return await job_crud.get_job_apps(db, skip=skip, limit=limit)
 
 
 @router.get("/{id}", response_model=job_schemas.JobApp)
+@router.get("/filter", response_model=list[job_schemas.JobAppOut])
+@router.get("/{id}", response_model=job_schemas.JobAppOut)
 async def read_jobs_by_id(id: int, db: AsyncSession = Depends(get_async_db)):
     job_app = await job_crud.get_job_app_by_id(db, id)
     return job_app
 
 
-@router.put("/{id}", response_model=job_schemas.JobApp, status_code=200)
+@router.put("/{id}", response_model=job_schemas.JobAppOut, status_code=200)
 async def update_jobs_by_id(
         id: int,
         # This injects a SQLAlchemy database session using FastAPIs dependency injection system.
